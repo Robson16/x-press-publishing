a83a3603e369a018c26826104c2926f00d6e1ec3
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-07-24 12:26:02 -0300

    feat: artist table creation

---
+++ migration.js
@@ -0,0 +1,13 @@
+const sqlite = require('sqlite3').verbose();
+
+const db = new sqlite.Database('./database.sqlite');
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS Artist (
+    id INTEGER NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    date_of_birth TEXT NOT NULL,
+    biography TEXT NOT NULL,
+    is_currently_employed INTEGER NOT NULL DEFAULT 1
+  )
+`);
