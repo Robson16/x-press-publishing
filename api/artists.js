7ea867db07ecc86c94011a065ea4dc5e0a58c95c
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-07-24 18:33:27 -0300

    feat: create new artist route

--- api/artists.js
+++ api/artists.js
@@ -40,4 +40,49 @@ artistsRouter.get('/:artistId', (req, res) => {
   res.status(200).json({ artist: req.artist });
 });
 
+artistsRouter.post('/', (req, res, next) => {
+  const { name, dateOfBirth, biography } = req.body.artist;
+
+  if (!name || !dateOfBirth || !biography) {
+    return res.status(400).send('Missing required fields');
+  }
+
+  const isCurrentlyEmployed = req.body.artist.isCurrentlyEmployed === 0 ? 0 : 1;
+
+  const sql = `
+    INSERT INTO Artist (
+      name,
+      date_of_birth,
+      biography,
+      is_currently_employed
+    ) VALUES (
+      $name,
+      $dateOfBirth,
+      $biography,
+      $isCurrentlyEmployed
+    )
+  `;
+
+  const values = {
+    $name: name,
+    $dateOfBirth: dateOfBirth,
+    $biography: biography,
+    $isCurrentlyEmployed: isCurrentlyEmployed
+  };
+
+  db.run(sql, values, function (error) {
+    if (error) {
+      return next(error);
+    }
+
+    db.get('SELECT * FROM Artist WHERE id = $id', { $id: this.lastID }, (error, artist) => {
+      if (error) {
+        return next(error);
+      }
+
+      res.status(201).json({ artist: artist });
+    });
+  });
+});
+
 module.exports = artistsRouter;
