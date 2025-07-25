27e3e5c11fba6592d4800be387cbaceac5c984ff
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-07-25 20:35:29 -0300

    fix: port expected by the front-end

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require("express");
 
 const apiRouter = require("./api/api.js");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
