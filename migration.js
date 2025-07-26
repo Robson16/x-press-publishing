0d03e9ad97e5e6a7a44b899954904552dcfdae5b
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-07-26 17:27:42 -0300

    feat: create issue table

--- migration.js
+++ migration.js
@@ -9,7 +9,7 @@ db.run(`
     date_of_birth TEXT NOT NULL,
     biography TEXT NOT NULL,
     is_currently_employed INTEGER NOT NULL DEFAULT 1
-  )
+  );
 `);
 
 db.run(`
@@ -17,5 +17,18 @@ db.run(`
     id INTEGER NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
     description TEXT NOT NULL
+  );
+`);
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS Issue (
+    id INTEGER NOT NULL PRIMARY KEY,
+    artist_id INTEGER NOT NULL,
+    series_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    issue_number INTEGER NOT NULL,
+    publication_date TEXT NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES Artist (id),
+    FOREIGN KEY (series_id) REFERENCES Series (id)
   );
 `);
