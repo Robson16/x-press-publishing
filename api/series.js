91f7391bc29b03ea389f9fcdc747b959bdd6343a
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-07-26 16:31:30 -0300

    feat: series route param

--- api/series.js
+++ api/series.js
@@ -5,6 +5,25 @@ const path = require('path');
 const db = new sqlite.Database(process.env.TEST_DATABASE || path.resolve(__dirname, '../database.sqlite'));
 const seriesRouter = express.Router();
 
+seriesRouter.param('seriesId', (req, res, next, seriesId) => {
+  const sql = 'SELECT * FROM Series WHERE id = $seriesId';
+  const values = { $seriesId: seriesId };
+
+  db.get(sql, values, (error, series) => {
+    if (error) {
+      return next(error);
+    }
+
+    if (!series) {
+      return res.status(404).send();
+    }
+
+    req.series = series;
+
+    next();
+  });
+});
+
 seriesRouter.get('/', (req, res, next) => {
   db.all(
     'SELECT * FROM Series',
