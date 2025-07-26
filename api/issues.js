3d85b620134a589cca27b4c7a078f08fdf179a9e
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-07-26 18:38:49 -0300

    feat: create issue route

--- api/issues.js
+++ api/issues.js
@@ -23,4 +23,60 @@ issuesRouter.get('/', (req, res, next) => {
   );
 });
 
+issuesRouter.post('/', (req, res, next) => {
+  const seriesId = req.params.seriesId;
+  const { name, issueNumber, publicationDate, artistId } = req.body.issue;
+
+  if (!name || !issueNumber || !publicationDate || !artistId) {
+    return res.status(400).send('Missing required fields');
+  }
+
+  const artistExists = db.get(
+    'SELECT * FROM Artist WHERE id = $artistId',
+    { $artistId: artistId }
+  );
+
+  if (!artistExists) {
+    return res.status(400).send('Artist not found');
+  }
+
+  const sql = `
+    INSERT INTO Issue (
+      name,
+      issue_number,
+      publication_date,
+      artist_id,
+      series_id
+    ) VALUES (
+      $name,
+      $issueNumber,
+      $publicationDate,
+      $artistId,
+      $seriesId
+    )
+  `;
+
+  const values = {
+    $name: name,
+    $issueNumber: issueNumber,
+    $publicationDate: publicationDate,
+    $artistId: artistId,
+    $seriesId: seriesId
+  };
+
+  db.run(sql, values, function (error) {
+    if (error) {
+      return next(error);
+    }
+
+    db.get('SELECT * FROM Issue WHERE id = $id', { $id: this.lastID }, (error, issue) => {
+      if (error) {
+        return next(error);
+      }
+
+      res.status(201).json({ issue: issue });
+    });
+  });
+});
+
 module.exports = issuesRouter;
